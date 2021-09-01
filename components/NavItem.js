e70d63b14b7bc84bfb1d91b33a13e2a1bbc36ca5
adam <adamgonlag@gmail.com>
2021-09-01 14:22:02 +1000

    convert to sass, implemet resume button animation

--- components/NavItem.js
+++ components/NavItem.js
@@ -1,4 +1,4 @@
-import navStyles from "../styles/Navbar.module.css";
+import navStyles from "../styles/Navbar.module.scss";
 import Link from "next/link";
 
 const NavItem = ({ item }) => {
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,28 @@
-import navStyles from "../styles/Navbar.module.css";
+import navStyles from "../styles/Navbar.module.scss";
 import React from "react";
 import NavItem from "./NavItem";
 
 const menuItems = [
-  { id: 1, name: "Home", slug: "/" },
-  { id: 2, name: "About", slug: "/about" },
-  { id: 3, name: "Experience", slug: "/experience" },
-  { id: 4, name: "Projects", slug: "/projects" },
-  { id: 5, name: "Contact", slug: "/contact" },
+  { id: 1, name: "About", slug: "/about" },
+  { id: 2, name: "Projects", slug: "/projects" },
+  { id: 3, name: "Contact", slug: "/contact" },
 ];
 
 const Navbar = () => {
   return (
     <nav className={navStyles.navbar}>
+      <div className={navStyles.navbar__logo}>AG</div>
       <ul className={navStyles.navbar__list}>
         {menuItems.map((item) => {
           return <NavItem key={item.id} item={item} />;
         })}
+        <li>
+          <button className={navStyles.navbar__button}>
+            <span>
+              <span>Resume</span>
+            </span>
+          </button>
+        </li>
       </ul>
     </nav>
   );
