8e9794038f40f2e060809063d7cf6636b67c7bd9
adam <adamgonlag@gmail.com>
2021-10-05 18:01:56 +1100

    fix bug: email not working in prod

--- pages/api/contact/index.js
+++ pages/api/contact/index.js
@@ -1,36 +1,38 @@
 let nodemailer = require("nodemailer");
 const nodemailerSendgrid = require("nodemailer-sendgrid");
 
-export default async function handler(req, res) {
-  require("dotenv").config();
-  const transport = nodemailer.createTransport(
-    nodemailerSendgrid({
-      apiKey: process.env.SENDGRID_API_KEY,
-    })
-  );
+export default async function handler(req, response) {
+  if (req.method === "POST") {
+    require("dotenv").config();
 
-  transport
-    .sendMail({
-      from: "adam@skyboxpro.net",
-      to: "adamgonlag+portfolio@gmail.com",
-      subject: `Message From ${req.body.name}`,
-      text: `From: ${req.body.email}, Name: ${req.body.name} Message: ${req.body.message}`,
-      html: `
-        <div>
-          <p>From: ${req.body.email}</p>
-          <p>Name: ${req.body.name}</p>
-          <p>Message: ${req.body.message}</p>
-        </div>
-      `,
-    })
-    .then(([res]) => {
+    const transport = nodemailer.createTransport(
+      nodemailerSendgrid({
+        apiKey: process.env.SENDGRID_API_KEY,
+      })
+    );
+
+    try {
+      const [res] = await transport.sendMail({
+        from: "adam@skyboxpro.net",
+        to: "adamgonlag+portfolio@gmail.com",
+        subject: `Message From ${req.body.name}`,
+        text: `From: ${req.body.email}, Name: ${req.body.name} Message: ${req.body.message}`,
+        html: `
+          <div>
+            <p>From: ${req.body.email}</p>
+            <p>Name: ${req.body.name}</p>
+            <p>Message: ${req.body.message}</p>
+          </div>
+        `,
+      });
+
       console.log(
         "Message delivered with code %s %s",
         res.statusCode,
         res.statusMessage
       );
-    })
-    .catch((err) => {
+      response.status(200).json(req.body);
+    } catch (error) {
       console.log("Errors occurred, failed to deliver message");
 
       if (err.response && err.response.body && err.response.body.errors) {
@@ -40,7 +42,8 @@ export default async function handler(req, res) {
       } else {
         console.log(err);
       }
-    });
 
-  res.status(200).json(req.body);
+      res.status(err).json({});
+    }
+  }
 }
