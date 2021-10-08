45e71f0e758e91242aff7fd5d97ca1f60b8ad6cf
adam <adamgonlag@gmail.com>
2021-10-08 22:38:40 +1100

    typo in about section

--- components/About.js
+++ components/About.js
@@ -3,7 +3,7 @@ import styles from "../styles/About.module.scss";
 
 export default function About() {
   const p1 = `Civil Engineer/Project Manager turned Software Developer, I have a passion for solving complex technical problems and working with great people to achieve awesome things.`;
-  const p2 = `I’m excited for the next stage in my career where I can continue to learn and use my engineering and project management skills to add value from day 1`;
+  const p2 = `I’m excited for the next stage in my career where I can continue to learn and use my engineering and project management skills to add value from day 1.`;
   const p3 = `When I'm not behind a screen, you'll find me playing music, taking care of the lawn or hitting the trails on a mountain bike.`;
   return (
     <section id="about" className={styles.section}>
